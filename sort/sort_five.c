3cdbe8cea68caf0423e5325dea86e674e911df97
Mohamed Amine Amazzal <mamazzal@c1r2p1.1337.ma>
2023-03-16 19:11:08 +0100

    init fix some bugs

--- sort/sort_five.c
+++ sort/sort_five.c
@@ -4,12 +4,15 @@ void sort_five_args() {
  int size = count_length_stack_a();
  int count = 0;
  while (count < size / 2) {
+  if (asccii_to_int(s_args.stack_a[count_length_stack_a() - 1]) == find_min_nbr()) {
+   bottom_number_go_to_up_and_push_down_by_one();
+  }
   if (asccii_to_int(s_args.stack_a[0]) == find_min_nbr()) {
    send_top_of_a_to_top_of_b();
    s_args.arr_size -= 1;
   }else {
    top_nbr_go_to_bottom_and_shift_up_by_one();
-   count--;
+   count-= 1;
   }
   count++;
  }
